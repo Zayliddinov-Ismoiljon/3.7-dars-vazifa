377fe6a80506a1067ece170c24383d7bce048a54
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2022-02-20 17:23:00 +0500

    Innitial commit

---
+++ Js/main.js
@@ -0,0 +1,11 @@
+var a= Number(prompt("Summani kiriting(so'm)"));
+b=Number(9647716.8);
+
+//b=500*10900+250*10900+120*12272.64
+
+if (a >= b){
+    console.log("Oq yo'l! Alisher");
+}
+else{
+    console.log("Biroz sabr qilishingiz kerak Alisher");
+}
